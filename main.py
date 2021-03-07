be519f629053aa1399a58c3d3565fd4c8ba4f6dd
JoshTheBlack <JoshTheBlack@gmail.com>
2021-03-07 16:56:54 -0500

    Not much progress

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import discord
 import os
 from dotenv import load_dotenv
-#from replit import db
+from tinydb import TinyDB, Query
 
 load_dotenv()
-
+db = TinyDB('db.json')
+User = Query()
 client = discord.Client()
+# db Format { player.name : [ player.name, player.wins, player.plays, player.played, player.active ] }
+
+class Player:
+    def __init__(self, name, wins, plays, played):
+        '''played is a list of games played'''
+        self.name = name.lower()
+        self.wins = wins
+        self.plays = plays
+        self.played = played
 
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
 
 @client.event
-async def on_message(message):
+async def on_message(message, db=db, User=User):
     if message.author == client.user:
         return
     
@@ -21,7 +31,15 @@ async def on_message(message):
         await message.channel.send(f'Hello! {user.split("#",1)[0]}')
     
     if message.content.startswith('!players'):
-        players = str(message.content).split(' ',1)[1].replace(' ','').split(',')
-        print(players)
+        players = str(message.content).split(' ',1)[1].replace(' ','').lower().split(',')
+        for player in players:
+            if db.search(User.name == player):
+                stored = db.get(User.name == str(player))
+                stored["active"] = True
+            else:
+                db.upsert({"name": player, "wins":0,"plays":0,"played": [],"active": True, "won":False}, User.name == player)
+
+
+
 
 client.run(os.getenv('TOKEN'))
