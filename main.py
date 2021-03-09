5554a7a7f6859c2a7f746947a138cd7a32818663
JoshTheBlack <JoshTheBlack@gmail.com>
2021-03-09 16:37:37 -0500

    Basic functionality complete!

--- main.py
+++ main.py
@@ -56,11 +56,25 @@ async def on_message(message, db=db, User=User):
                 if game not in player["played"]:
                     player["played"][game] = {"wins" : 0, "plays" : 0}
                 player["played"][game]["plays"] += 1
-                if player["name"] == winner:
+                if player["name"] in winner:
                     player["wins"] += 1
                     player["played"][game]["wins"] += 1
             db.upsert(player,User.name == player["name"])
         print(db.all())
+
+    def player_stats(playername, game="all"):
+        player = db.get(User.name == playername)
+        games = "\n"
+        if game == "all":
+            for i in player["played"]:
+                games += f'{player["name"]} has won **{player["played"][i]["wins"]}** of **{player["played"][i]["plays"]}** games of **{i}**\n'
+        else:
+            for i in game:
+                try:
+                    games += f'{player["name"]} has won **{player["played"][i]["wins"]}** of **{player["played"][i]["plays"]}** games of **{i}**\n'
+                except:
+                    games += f'{player["name"]} has not recorded any plays of {i}\n'
+        return f"""{player["name"]} has won **{player["wins"]}** of all **{player["plays"]}** games played.{games}\n"""
 
     if message.author == client.user:
         return
@@ -76,18 +90,30 @@ async def on_message(message, db=db, User=User):
         await message.channel.send(f'Active players set to {*players,}')
         
     if message.content.startswith('!record'):
-        game = str(message.content).split(' ',1)[1].lower().split(',',1)[0].strip()
-        winner = str(message.content).split(' ',1)[1].lower().split(',',1)[1].strip()
+        winner = str(message.content).split(' ',2)[1].lower().split(',')
+        game = str(message.content).split(' ',2)[2].lower()
         record_play(game,winner)
         currentplayers = get_active_players()
-        await message.channel.send(f'Recorded {winner} as the winner of {game}. Updated playcounts for {currentplayers}')
+        await message.channel.send(f'Recorded {winner} as the winner(s) of {game}. Updated playcounts for {currentplayers}')
 
     if message.content.startswith('!help'):
         response = """Use "!players" to set active players.  e.g. "!players adam,breanna,caleb,dakota"\n
-        Use "!record (gamename), (winner)" to record a game for all active players.  e.g. "!record Sagrada, Dakota" \n
-        Use "!stats playername" to see stats for a player.  e.g. !stats breanna"
+        Use "!record (comma-separated winner names), (comma-separated game names)" to record a game for all active players.  e.g. "!record Dakota Sagrada" \n
+        Use "!stats playername (optional)gamename" to see stats for a player.  e.g. !stats breanna or !stats breanna,dakota sagrada,6 nimmt"
         """
         await message.channel.send(response)
+    
+    if message.content.startswith('!stats'):
+        currentplayers = str(message.content).split(' ',2)[1].replace(' ','').lower().split(',')
+        try:
+            game = str(message.content).split(' ',2)[2].lower().split(',')
+        except:
+            game = "all"
+        stats = ""
+        for currentplayer in currentplayers:
+            stats += player_stats(currentplayer,game)
+        await message.channel.send(stats)
+
 
 
 
