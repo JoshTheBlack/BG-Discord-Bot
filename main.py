96440afdaa4bbddd28216d88079a51774b40cfd3
JoshTheBlack <JoshTheBlack@gmail.com>
2021-03-09 13:38:53 -0500

    Can update playcount and change players

--- main.py
+++ main.py
@@ -23,6 +23,42 @@ async def on_ready():
 
 @client.event
 async def on_message(message, db=db, User=User):
+    def set_players(players):
+        for player in players:
+            if db.search(User.name == player):
+                stored = db.get(User.name == str(player))
+                stored["active"] = True
+                db.upsert(stored, User.name == player)
+            else:
+                db.upsert({"name": player, "wins":0,"plays":0,"played": [],"active": True, "won":False}, User.name == player)
+        print(db.all())
+
+    def reset_players():
+        players = db.all()
+        for player in players:
+            player["active"] = False
+            db.upsert(player,User.name == player["name"])
+        print(db.all())
+
+    def get_active_players():
+        activePlayers = []
+        base = db.all()
+        for player in base:
+            if player["active"] == True:
+                activePlayers.append(player["name"])
+
+    def record_play(game, winner):
+        players = db.all()
+        for player in players:
+            if player["active"] == True:
+                player["plays"] += 1
+                if game not in player["played"]:
+                    player["played"].append(game)
+                if player["name"] == winner:
+                    player["wins"] += 1
+            db.upsert(player,User.name == player["name"])
+        print(db.all())
+
     if message.author == client.user:
         return
     
@@ -31,13 +67,18 @@ async def on_message(message, db=db, User=User):
         await message.channel.send(f'Hello! {user.split("#",1)[0]}')
     
     if message.content.startswith('!players'):
+        reset_players()
         players = str(message.content).split(' ',1)[1].replace(' ','').lower().split(',')
-        for player in players:
-            if db.search(User.name == player):
-                stored = db.get(User.name == str(player))
-                stored["active"] = True
-            else:
-                db.upsert({"name": player, "wins":0,"plays":0,"played": [],"active": True, "won":False}, User.name == player)
+        set_players(players)
+        await message.channel.send(f'Active players set to {*players,}')
+        
+    if message.content.startswith('!record'):
+        game = str(message.content).split(' ',1)[1].lower().split(',',1)[0]
+        winner = str(message.content).split(' ',1)[1].lower().split(',',1)[1]
+        record_play(game,winner)
+        currentplayers = get_active_players()
+        await message.channel.send(f'Recorded {winner} as the winner of {game}. Updated playcounts for {currentplayers}')
+
 
 
 
