fec08130bc8b1b6b380d1ad9427aec0d82963f0b
JoshTheBlack <JoshTheBlack@gmail.com>
2021-03-10 13:02:26 -0500

    Added attendance

--- main.py
+++ main.py
@@ -2,20 +2,13 @@ import discord
 import os
 from dotenv import load_dotenv
 from tinydb import TinyDB, Query
+from datetime import datetime
 
 load_dotenv()
 db = TinyDB('db.json')
 User = Query()
 client = discord.Client()
-# db Format { player.name : [ player.name, player.wins, player.plays, player.played = {"game" : {wins, plays}}, player.active ] }
-
-class Player:
-    def __init__(self, name, wins, plays, played):
-        '''played is a list of games played'''
-        self.name = name.lower()
-        self.wins = wins
-        self.plays = plays
-        self.played = played
+# db Format { player.name : [ player.name, player.wins, player.plays, player.played = {"game" : {wins, plays}}, player.active, attendance = [date, date, date...] }
 
 @client.event
 async def on_ready():
@@ -30,7 +23,7 @@ async def on_message(message, db=db, User=User):
                 stored["active"] = True
                 db.upsert(stored, User.name == player)
             else:
-                db.upsert({"name": player, "wins":0,"plays":0,"played": {},"active": True}, User.name == player)
+                db.upsert({"name": player, "wins":0,"plays":0,"played": {},"active": True, "attendance": []}, User.name == player)
         print(db.all())
 
     def reset_players():
@@ -53,6 +46,9 @@ async def on_message(message, db=db, User=User):
         for player in players:
             if player["active"] == True:
                 player["plays"] += 1
+                date = datetime.today().strftime('%Y-%m-%d')
+                if date not in player["attendance"]:
+                    player["attendance"].append(date)
                 if game not in player["played"]:
                     player["played"][game] = {"wins" : 0, "plays" : 0}
                 player["played"][game]["plays"] += 1
@@ -67,14 +63,20 @@ async def on_message(message, db=db, User=User):
         games = "\n"
         if game == "all":
             for i in player["played"]:
-                games += f'{player["name"]} has won **{player["played"][i]["wins"]}** of **{player["played"][i]["plays"]}** games of **{i}**\n'
+                games += f'**{player["name"]}** has won **{player["played"][i]["wins"]}** of **{player["played"][i]["plays"]}** games of **{i}**\n'
         else:
             for i in game:
                 try:
-                    games += f'{player["name"]} has won **{player["played"][i]["wins"]}** of **{player["played"][i]["plays"]}** games of **{i}**\n'
+                    games += f'**{player["name"]}** has won **{player["played"][i]["wins"]}** of **{player["played"][i]["plays"]}** games of **{i}**\n'
                 except:
-                    games += f'{player["name"]} has not recorded any plays of {i}\n'
-        return f"""{player["name"]} has won **{player["wins"]}** of all **{player["plays"]}** games played.{games}\n"""
+                    games += f'**{player["name"]}** has not recorded any plays of {i}\n'
+        return f"""**{player["name"]}** has won **{player["wins"]}** of all **{player["plays"]}** games played.{games}\n"""
+
+    def get_attendance(playername):
+        player = db.get(User.name == playername)
+        if player == None:
+            return f'**{playername}** has **no** recorded game sessions.'
+        return f'**{playername}** has attended **{len(player["attendance"])}** recorded game session{"s" if len(player["attendance"]) != 1 else ""}.\n{player["attendance"]}'
 
     if message.author == client.user:
         return
@@ -109,12 +111,14 @@ async def on_message(message, db=db, User=User):
             game = str(message.content).split(' ',2)[2].lower().split(',')
         except:
             game = "all"
-        stats = ""
+        #stats = ""
         for currentplayer in currentplayers:
-            stats += player_stats(currentplayer,game)
-        await message.channel.send(stats)
+            #stats += player_stats(currentplayer,game)
+            await message.channel.send(player_stats(currentplayer,game))
 
-
-
+    if message.content.startswith('!attendance'):
+        currentplayers = str(message.content).split(' ',1)[1].replace(' ','').lower().split(',')
+        for player in currentplayers:
+            await message.channel.send(get_attendance(player))
 
 client.run(os.getenv('TOKEN'))
